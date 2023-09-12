75d24e0b6375069e82fd13ccad22f112a00619ad
John Nelson <johne.nelsoniv@gmail.com>
2023-09-12 19:13:03 -0400

    clean up comments

--- vendingreciever.java
+++ vendingreciever.java
@@ -1,29 +1,15 @@
 /*
-You are to write simple coin-receiver for a soda
-vending machine. Here are the specifications:
-1. Now that you learned about commenting in Section 4.9 of the textbook, all
-programs submitted going forward should have a heading of the following
-format:
+
 
 A descriptive sentence of what this file does.
-@author your_name_here
-@collaborator any_partner_you_worked_with
+
+This files takes a string input (P,N,D,Q,H,G) and converts it to the appropriate dollar (cents) amount (.01, .05, .10, .25, .50, 1.00)
+and then udpates the overall machine coin balance.
+
+@author Victoria Fabrizio
+@collaborator John Nelson
 
 
-2. The program should set up an empty variable for the amount of money placed
-in the machine by the user. (Initially, none.)
-3. The program should prompt "Please enter a coin :>".
-4. The program should then accept any of the following letters: P (penny), N
-(nickel), D (dime), Q (quarter), H (half dollar), G (golden dollar coin). The
-machine should accept input of either case (i.e., upper or lower).
-5. The program should update the machine's balance with what was entered by
-the user.
-6. The program should print: "Your balance is []." The [] should be replaced by a
-properly formatted $ amount using decimals.
-7. Only one coin is to be dropped in at this point.
-8. This is revision one of the program.
-9. Also generate the Javadoc for this version of the program.
-10. Push the source code and documentation to your Github repository.
 */
 import java.util.*;
 
