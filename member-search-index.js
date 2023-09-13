6b8a29de145107ffc2cfa1fa14427a2a9ed495ef
John Nelson <johne.nelsoniv@gmail.com>
2023-09-12 20:37:16 -0400

    adding this mess of a javadoc file to the branched repo

---
+++ member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"vendingreciever","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"vendingreciever","l":"vendingreciever()","u":"%3Cinit%3E()"}];updateSearchResults();
